78fce55bb7d6cd2070ccbab06210ad4ab5809fb6
Enrico Speranza <enrico.speranza@gmail.com>
2024-10-31 11:45:33 +0000

    Final working example

--- main.go
+++ main.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/batchatco/go-native-netcdf/netcdf"
 	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
+	//-20.602992, -23.594518 //https://maps.app.goo.gl/TGBuqiGsmjKky2SE7
+	//Depth example -347 m
+
+	depth := -87.0
+	lat := -20.602992
+	long := -23.594518
+
+	var idepth int
+	depth = math.Abs(depth)
+	if depth > 0 && depth <= 100 {
+		//Convert to integer positive
+		idepth = int(math.Abs(depth)) / 5
+	} else {
+		idepth = int(math.Abs(depth)) / 25
+	}
+	fmt.Printf("Depth array index: %d (%f)\n", idepth, depth)
+
+	var ilat int
+	if lat < 90.0 {
+		ilat = int((90.0 - (math.Abs(math.Round(lat)))))
+	} else {
+		ilat = int(math.Round(lat))
+	}
+	fmt.Printf("Depth array lat: %d (%f)\n", ilat, lat)
+
+	var ilong int
+	if long < 180.0 {
+		ilong = int(180.0-(math.Abs(math.Round(long))))
+	} else {
+		ilong = int(math.Round(long))
+	}
+	fmt.Printf("Depth array long: %d (%f)\n", ilong, long)
+
 	// Open the file
 	nc, err := netcdf.Open("woa23_decav_s01_01.nc")
 	if err != nil {
@@ -22,7 +56,7 @@ func main() {
 	}
 
 	fmt.Println("------ VARIABLES ---------")
-	
+
 	// List all variables
 	vl := nc.ListVariables()
 	for i, v := range vl {
@@ -54,20 +88,21 @@ func main() {
 
 	vssdd, _ := vs.Values()
 	vsddd := vssdd.([][][][]float32)
-	spew.Dump(vsddd[0])
-	
+	for i := 0; i < 57; i++ {
+		spew.Dump(vsddd[0][i][ilat][ilong])
+	}
 	//See this:
 	//https://github.com/batchatco/go-native-netcdf/blob/5849c1f424b12bc9f6441723ba6297f2d484e5d2/netcdf/cdf/slicer_test.go
 
 	fmt.Println("------ VALUES ---------")
 	/*
-	// Cast the data into a Go type we can use
-	lats, has := vr.Values.([]float32)
-	if !has {
-		panic("latitude data not found")
-	}
-	for i, lat := range lats {
-		fmt.Println(i, lat)
-	}
+		// Cast the data into a Go type we can use
+		lats, has := vr.Values.([]float32)
+		if !has {
+			panic("latitude data not found")
+		}
+		for i, lat := range lats {
+			fmt.Println(i, lat)
+		}
 	*/
 }
